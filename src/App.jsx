8a7cb9a33f582e3d5af471da3cd898875e189f47
unknown <ejboland97@gmail.com>
2023-12-15 00:04:34 -0500

    updated app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Nav from './components/HeaderNavigation';
 import Footer from './components/Footer';
 
-function App() {
-  
+const App = () => {
+  const mainStyle = {
+    background: 'linear-gradient(to bottom, #273469, #1E2749)',
+    color: 'white',
+    minHeight: '100vh',
+  };
+
   return (
     <>
       <Nav />
-      <main style={{ 
-        background: 'linear-gradient(to bottom, #273469, #1E2749)',
-        color: 'white',
-        minHeight: '100vh', 
-      }}>
+      <main style={mainStyle}>
         <Outlet />
       </main>
       <Footer />
     </>
   );
-}
+};
 
 export default App;
 
