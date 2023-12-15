3af8b519d6f971f088d35cdd93968549b9f2fd19
unknown <ejboland97@gmail.com>
2023-12-15 00:06:36 -0500

    updated error handling

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,35 +1,26 @@
-// import { useRouteError } from 'react-router-dom';
-
-// export default function ErrorPage() {
-//   const error = useRouteError();
-//   console.error(error);
-
-//   return (
-//     <div>
-//       <h1>Error</h1>
-//       <p>An unforseen error has occurred.</p>
-//       <p>
-//         <i>{error.statusText || error.message}</i>
-//       </p>
-//     </div>
-//   );
-// }
-
-
 import { useRouteError } from 'react-router-dom';
 
+// ErrorPage component for displaying error information
 const ErrorPage = () => {
+  // Retrieve the error information from the route
   const error = useRouteError();
+
+  // Log the error to the console for debugging purposes
   console.error(error);
 
+  // JSX rendering of the ErrorPage component
   return (
     <div className="error-container">
+      {/* Header section */}
       <header>
         <h1>Oops! Something Went Wrong</h1>
       </header>
+      {/* Main content section */}
       <section>
+        {/* Error message */}
         <p>We encountered an unexpected error.</p>
         <p>Please try again later or contact support.</p>
+        {/* Display the status text or message from the error */}
         <p>
           <i>{error.statusText || error.message}</i>
         </p>
@@ -38,4 +29,5 @@ const ErrorPage = () => {
   );
 };
 
+// Export the ErrorPage component as the default export
 export default ErrorPage;
